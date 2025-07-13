c00c0658effd03e59da427b4099a71f78158c3c0
Giovanna Sales <117942218+giossales@users.noreply.github.com>
2025-07-13 14:13:55 -0400

    Create helpers.ts

---
+++ ExpTSFinal/src/helpers/helpers.ts
@@ -0,0 +1,20 @@
+import { DiscType, TechType } from "./helpersTypes";
+
+export function discList(discs: DiscType[]) {
+	return `<ul>${(discs
+		.map((d) => `<li>${d.nome} - ${d.cg}</li>`))
+		.join("")}</ul>`
+}
+
+export function techList(techs: TechType[]) {
+	return `<ul>${(techs
+		.map((t) => {
+			if (t.poweredByNodejs) return `<li>${t.name} - ${t.type}</li>`
+			else return ''
+		}))
+		.join("")}</ul>`
+}
+
+export function inc(value: any) {
+	return parseInt(value) + 1;
+}
